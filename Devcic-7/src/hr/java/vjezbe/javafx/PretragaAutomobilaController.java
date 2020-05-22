c9f99fe687991e4b8b456984ac44309d2652a6fa
kdevcic77 <32877893+kdevcic77@users.noreply.github.com>
2020-05-22 08:17:16 +0200

    Exercise finished

--- Devcic-7/src/hr/java/vjezbe/javafx/PretragaAutomobilaController.java
+++ Devcic-7/src/hr/java/vjezbe/javafx/PretragaAutomobilaController.java
@@ -1,6 +1,7 @@
 package hr.java.vjezbe.javafx;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,11 +11,36 @@ import hr.java.vjezbe.util.Datoteke;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+
 public class PretragaAutomobilaController {
+    @FXML
+    private Label naslov;
+    @FXML
+    private Label opis;
+    @FXML
+    private Label snakaKs;
+    @FXML
+    private Label cijena;
+    @FXML
+    private TextField txtFieldNaslov;
+    @FXML
+    private TextField txtFieldOpis;
+    @FXML
+    private TextField txtFieldSnagaKs;
+    @FXML
+    private TextField txtFieldCijena;
+    @FXML
+    private Button buttonPretraga;
+
     @FXML
     private TableView<Automobil> automobilTableView;
     @FXML
@@ -29,6 +55,8 @@ public class PretragaAutomobilaController {
     private TableColumn<Automobil, BigDecimal> cijenaTableColumn;
 
 
+    List<Automobil> listaAutomobila = new ArrayList<>();
+
     public void initialize() {
 	naslovTableColumn.setCellValueFactory(new PropertyValueFactory<Automobil, String>("naslov"));
 	opisTableColumn.setCellValueFactory(new PropertyValueFactory<Automobil, String>("opis"));
@@ -38,9 +66,60 @@ public class PretragaAutomobilaController {
 
 	List<Automobil> listaAutomobila = Datoteke.dohvatiArtikle().stream().filter(p -> p instanceof Automobil)
 		.map(sc -> (Automobil) sc).collect(Collectors.toList());
-	
+
 	ObservableList<Automobil> listaAutomobilaObservable = FXCollections.observableArrayList(listaAutomobila);
-	
+
 	automobilTableView.setItems(listaAutomobilaObservable);
     }
+
+    public void pretraziAutomobile() {
+	List<Automobil> listaAutomobila = Datoteke.dohvatiArtikle().stream().filter(p -> p instanceof Automobil)
+		.map(sc -> (Automobil) sc).collect(Collectors.toList());
+	List<Automobil> privremenaLista = new ArrayList<>();
+	List<Automobil> filtriraniAutomobili = new ArrayList<>();
+
+	if (txtFieldNaslov.getText().isEmpty() == false) {
+	    privremenaLista = (listaAutomobila.stream()
+		    .filter(a -> a.getNaslov().toLowerCase().contains(txtFieldNaslov.getText().toLowerCase()))
+		    .collect(Collectors.toList()));
+	    filtriraniAutomobili.clear();
+	    filtriraniAutomobili.addAll(privremenaLista);
+	}
+	if (txtFieldOpis.getText().isEmpty() == false) {
+	    privremenaLista = listaAutomobila.stream()
+		    .filter(a -> a.getOpis().toLowerCase().contains(txtFieldOpis.getText().toLowerCase()))
+		    .collect(Collectors.toList());
+	    filtriraniAutomobili.clear();
+	    filtriraniAutomobili.addAll(privremenaLista);
+	}
+	if (txtFieldSnagaKs.getText().isEmpty() == false) {
+	    privremenaLista = listaAutomobila.stream()
+		    .filter(a -> a.getSnagaKs().toString().contains(txtFieldSnagaKs.getText()))
+		    .collect(Collectors.toList());
+	    filtriraniAutomobili.clear();
+	    filtriraniAutomobili.addAll(privremenaLista);
+	}
+
+	if (txtFieldCijena.getText().isEmpty() == false) {
+	    privremenaLista = listaAutomobila.stream()
+		    .filter(a -> a.getCijena().toString().contains(txtFieldCijena.getText()))
+		    .collect(Collectors.toList());
+	    filtriraniAutomobili.clear();
+	    filtriraniAutomobili.addAll(privremenaLista);
+	}
+
+	if (filtriraniAutomobili.size() == 0) {
+	    filtriraniAutomobili.addAll(listaAutomobila);
+	    Alert alert = new Alert(AlertType.INFORMATION);
+	    alert.setTitle("Upozorenje");
+	    alert.setHeaderText("Ne postoji ništa pod tim filterom");
+	    alert.setContentText("Nije stavljen nijedan filter");
+	    alert.showAndWait();
+
+	}
+
+	ObservableList<Automobil> listaFiltriranihAutomobilaObservable = FXCollections.observableArrayList(filtriraniAutomobili);
+	automobilTableView.setItems(listaFiltriranihAutomobilaObservable);
+    }
+
 }
--- Devcic-7/src/hr/java/vjezbe/javafx/PretragaPoslovnihKorisnikaController.java
+++ Devcic-7/src/hr/java/vjezbe/javafx/PretragaPoslovnihKorisnikaController.java
@@ -1,5 +1,6 @@
 package hr.java.vjezbe.javafx;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,34 @@ import hr.java.vjezbe.util.Datoteke;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PretragaPoslovnihKorisnikaController {
+    @FXML
+    private Label naziv;
+    @FXML
+    private Label web;
+    @FXML
+    private Label email;
+    @FXML
+    private Label telefon;
+    @FXML
+    private TextField txtFieldNaziv;
+    @FXML
+    private TextField txtFieldWeb;
+    @FXML
+    private TextField txtFieldEmail;
+    @FXML
+    private TextField txtFieldTelefon;
+    @FXML
+    private Button buttonPretraga;
     @FXML
     private TableView<PoslovniKorisnik> poslovniKorisnikTableView;
     @FXML
@@ -30,12 +54,61 @@ public class PretragaPoslovnihKorisnikaController {
 	emailTableColumn.setCellValueFactory(new PropertyValueFactory<PoslovniKorisnik, String>("email"));
 	telefonTableColumn.setCellValueFactory(new PropertyValueFactory<PoslovniKorisnik, String>("telefon"));
 
-
-	List<PoslovniKorisnik> listaPoslovnihKorisnika = Datoteke.dohvatiKorisnike().stream().filter(p -> p instanceof PoslovniKorisnik).map(sc -> (PoslovniKorisnik) sc)
+	List<PoslovniKorisnik> listaPoslovnihKorisnika = Datoteke.dohvatiKorisnike().stream()
+		.filter(p -> p instanceof PoslovniKorisnik).map(sc -> (PoslovniKorisnik) sc)
 		.collect(Collectors.toList());
-
-	ObservableList<PoslovniKorisnik> listaPoslovnihKorisnikaObservable = FXCollections.observableArrayList(listaPoslovnihKorisnika);
-
+	ObservableList<PoslovniKorisnik> listaPoslovnihKorisnikaObservable = FXCollections
+		.observableArrayList(listaPoslovnihKorisnika);
 	poslovniKorisnikTableView.setItems(listaPoslovnihKorisnikaObservable);
+    }
+
+    public void pretraziPoslovneKorisnike() {
+	List<PoslovniKorisnik> listaPoslovnihKorisnika = Datoteke.dohvatiKorisnike().stream()
+		.filter(p -> p instanceof PoslovniKorisnik).map(sc -> (PoslovniKorisnik) sc)
+		.collect(Collectors.toList());
+	List<PoslovniKorisnik> privremenaLista = new ArrayList<>();
+	List<PoslovniKorisnik> filtiraniPoslovniKorisnici = new ArrayList<>();
+	if (txtFieldNaziv.getText().isEmpty() == false) {
+	    privremenaLista = (listaPoslovnihKorisnika.stream()
+		    .filter(a -> a.getNaziv().toLowerCase().contains(txtFieldNaziv.getText().toLowerCase()))
+		    .collect(Collectors.toList()));
+	    filtiraniPoslovniKorisnici.clear();
+	    filtiraniPoslovniKorisnici.addAll(privremenaLista);
+	}
+	if (txtFieldWeb.getText().isEmpty() == false) {
+	    privremenaLista = listaPoslovnihKorisnika.stream()
+		    .filter(a -> a.getWeb().toLowerCase().contains(txtFieldWeb.getText().toLowerCase()))
+		    .collect(Collectors.toList());
+	    filtiraniPoslovniKorisnici.clear();
+	    filtiraniPoslovniKorisnici.addAll(privremenaLista);
+	}
+	if (txtFieldEmail.getText().isEmpty() == false) {
+	    privremenaLista = listaPoslovnihKorisnika.stream()
+		    .filter(a -> a.getEmail().toLowerCase().contains(txtFieldEmail.getText()))
+		    .collect(Collectors.toList());
+	    filtiraniPoslovniKorisnici.clear();
+	    filtiraniPoslovniKorisnici.addAll(privremenaLista);
+	}
+
+	if (txtFieldTelefon.getText().isEmpty() == false) {
+	    privremenaLista = listaPoslovnihKorisnika.stream()
+		    .filter(a -> a.getTelefon().contains(txtFieldTelefon.getText())).collect(Collectors.toList());
+	    filtiraniPoslovniKorisnici.clear();
+	    filtiraniPoslovniKorisnici.addAll(privremenaLista);
+	}
+
+	if (filtiraniPoslovniKorisnici.size() == 0) {
+	    filtiraniPoslovniKorisnici.addAll(listaPoslovnihKorisnika);
+	    Alert alert = new Alert(AlertType.INFORMATION);
+	    alert.setTitle("Upozorenje");
+	    alert.setHeaderText("Ne postoji ništa pod tim filterom");
+	    alert.setContentText("Nije stavljen nijedan filter");
+	    alert.showAndWait();
+
+	}
+
+	ObservableList<PoslovniKorisnik> listaFiltriranihPoslovnihKorisnikaObservable = FXCollections
+		.observableArrayList(filtiraniPoslovniKorisnici);
+	poslovniKorisnikTableView.setItems(listaFiltriranihPoslovnihKorisnikaObservable);
     }
 }
